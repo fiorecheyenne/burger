654e8660276bf2d11b3eb5a5b436be9a3449a197
Hecate <fioriecheyenne@gmail.com>
2019-05-20 15:50:25 -0700

    fixing heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,40 @@
 var mysql = require("mysql");
+var connect;
+// var connect = mysql.createConnection({
+//   host: "localhost",
+//   port: 8080,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
 
-var connect = mysql.createConnection({
-  host: "localhost",
-  // port: 8080,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+// connect.connect(function(err) {
+//   if (err) {
+//     console.log("oops, error: " + err.stack);
+//     return;
+//   }
+//   console.log("Connected as " + connect.threadId);
+// });
+
+// module.exports = connect;
+
+if (process.env.JAWSDB_URL) {
+  connect = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connect = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 
 connect.connect(function(err) {
   if (err) {
-    console.log("oops, error: " + err.stack);
+    console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("Connected as " + connect.threadId);
+  console.log("connected as id " + connect.threadId);
 });
 
 module.exports = connect;
